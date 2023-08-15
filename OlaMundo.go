9272fa74ab65b5e4615eddd11d7679031269c12f
josedesignerbr@hotmail.com <imjosephe>
2023-08-15 17:25:32 -0400

    Novas funcoes

--- OlaMundo.go
+++ OlaMundo.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
+
+type Localizacao struct {
+	Latitude, Longitude float64
+}
+
+var frutas = []string{"Mamão", "Abacaxi", "Uva", "Banana", "Morango", "Melão", "Maçã", "Pera", "Limão"}
 
 func main() {
 	var opcao int
@@ -14,6 +23,10 @@ func main() {
 	3 - Range
 	4 - Calcular area retangulo
 	5 - Alterar pessoa por endereço
+	6 - Pegar parte das frutas
+	7 - Montar mapa
+	8 - Exibir localização
+	9 - Abrir arquivo de texto
 	0 - Sair`)
 
 	switch opcao {
@@ -27,8 +40,61 @@ func main() {
 		AreaRetangulo()
 	case 5:
 		AlterarPessoaPorEndereco()
+	case 6:
+		PegarParteDasFrutas()
+	case 7:
+		MontarMapa()
+	case 8:
+		ExibirLocalizacao()
+	case 9:
+		AbrirArquivoTexto()
 	default:
 		break
+	}
+}
+
+func AbrirArquivoTexto() {
+	var caminho = InputStr("Informe o caminho do arquivo de texto")
+	if strings.ToLower(filepath.Ext(caminho)) != ".txt" {
+		fmt.Println("Formato de arquivo inválido")
+		return
+	}
+
+	var arquivo, erroOpen = os.Open(caminho)
+	if erroOpen != nil {
+		fmt.Printf("Erro ao abrir o arquivo: %s\n", erroOpen)
+		return
+	}
+	defer arquivo.Close()
+
+	var conteudo, erroRead = io.ReadAll(arquivo)
+	if erroRead != nil {
+		fmt.Printf("Erro ao ler o arquivo: %s\n", erroRead)
+		return
+	}
+
+	fmt.Printf("Conteúdo do arquivo:\n%s", string(conteudo))
+}
+
+func ExibirLocalizacao() {
+	var latitude, longitude = InputFloat("Informe a latitude"), InputFloat("Informe a longitude")
+	fmt.Println(Localizacao{latitude, longitude})
+}
+
+func MontarMapa() {
+	var timeZones = map[string]int{
+		"UTC": 0 * 60 * 60,
+		"EST": -5 * 60 * 60,
+		"CST": -6 * 60 * 60,
+	}
+	var zona = InputStr("Informe a zona desejada (UTC, EST, CST)")
+	fmt.Println(timeZones[zona])
+}
+
+func PegarParteDasFrutas() {
+	var qtdFrutas = InputInt("Informe a quantidade de frutas")
+	for _, fruta := range frutas[0:qtdFrutas] {
+		fmt.Printf("%s\n", fruta)
 	}
 }
 
@@ -46,9 +112,8 @@ func AlterarPessoa(nome *string, idade *int) {
 }
 
 func AreaRetangulo() {
-	var base, altura, area, parametro int
-	base = InputInt("Informe a base")
-	altura = InputInt("Informe a altura")
+	var base, altura = InputInt("Informe a base"), InputInt("Informe a altura")
+	var area, parametro int
 	area, parametro = GetAreaRetangulo(base, altura)
 	fmt.Printf("Área: %d\nParametro: %d", area, parametro)
 }
@@ -60,26 +125,22 @@ func GetAreaRetangulo(base, altura int) (area, parametro int) {
 }
 
 func PercorrerVetor() {
-	var cpfs = []string{"12347203842", "45348264821", "96402047222", "98660638393"}
-	var nomes = []string{"Alberto", "Marcelo", "Roberto", "Patricia"}
+	var cpfs = [4]string{"12347203842", "45348264821", "96402047222", "98660638393"}
+	var nomes = [4]string{"Alberto", "Marcelo", "Roberto", "Patricia"}
 	for i, cpf := range cpfs {
 		fmt.Printf("cpf: %s nome: %s\n", cpf, nomes[i])
 	}
 }
 
 func Tabuada() {
-	var valor, limite int
-	valor = InputInt("Informe o valor da tabuada")
-	limite = InputInt("Informe o limite")
+	var valor, limite = InputInt("Informe o valor da tabuada"), InputInt("Informe o limite")
 	for i := 1; i <= limite; i++ {
 		fmt.Printf("%d X %d = %d\n", valor, i, i*valor)
 	}
 }
 
 func Soma() {
-	var valor1, valor2 float64
-	valor1 = InputFloat("Informe um valor")
-	valor2 = InputFloat("Informe outro valor")
+	var valor1, valor2 = InputFloat("Informe um valor"), InputFloat("Informe outro valor")
 	fmt.Printf("%.2f + %.2f é igual a %.2f", valor1, valor2, valor1+valor2)
 }
 
@@ -91,11 +152,9 @@ func InputStr(msg string) string {
 }
 
 func InputFloat(msg string) float64 {
-	var erro error
-	var result float64
-	result, erro = strconv.ParseFloat(strings.Replace(InputStr(msg), ",", ".", -1), 64)
+	var result, err = strconv.ParseFloat(strings.Replace(InputStr(msg), ",", ".", -1), 64)
 
-	if erro != nil {
+	if err != nil {
 		fmt.Println("Valor inválido")
 		return InputFloat(msg)
 	} else {
@@ -104,11 +163,9 @@ func InputFloat(msg string) float64 {
 }
 
 func InputInt(msg string) int {
-	var erro error
-	var result int
-	result, erro = strconv.Atoi(strings.Replace(InputStr(msg), ",", ".", -1))
+	var result, err = strconv.Atoi(strings.Replace(InputStr(msg), ",", ".", -1))
 
-	if erro != nil {
+	if err != nil {
 		fmt.Println("Valor inválido")
 		return InputInt(msg)
 	} else {
